348ec1cd956ef9b081fb334446e442794511fe82
dmi3 <dima@dmi3.net>
2020-02-11 10:32:10 +0200

    Fix for OS without `XDG_CONFIG_HOME` defined

--- config.fish
+++ config.fish
@@ -2,6 +2,8 @@
 #  
 #  Author: [Dmitry](http://dmi3.net) [Source](https://github.com/dmi3/fish)
 
+set -q XDG_CONFIG_HOME || set XDG_CONFIG_HOME "$HOME/.config"
+
 source $XDG_CONFIG_HOME/fish/main.fish
 source $XDG_CONFIG_HOME/fish/colors.fish
 source $XDG_CONFIG_HOME/fish/aliases.fish
